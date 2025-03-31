eaa3c7a1e30b2eb97f846bbe9d26a43ad21c4092
gwigz <gwigz@users.noreply.github.com>
2025-03-31 12:43:21 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center">
+			<h1 className="font-bold text-4xl">slua.tips</h1>
+		</main>
+	);
+}

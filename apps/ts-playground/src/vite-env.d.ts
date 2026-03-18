a9b982dbdedc6b1e24b99fe53d6870b50c316f7b
gwigz <gwigz@users.noreply.github.com>
2026-03-18 09:16:13 +0000

    fix(playground): typecheck failure on build

--- apps/ts-playground/src/vite-env.d.ts
+++ apps/ts-playground/src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+declare module "path-browserify" {
+  import path from "path"
+  export = path
+}
+
 declare module "virtual:tstl-lualib" {
   const files: Record<string, string>
   export default files
